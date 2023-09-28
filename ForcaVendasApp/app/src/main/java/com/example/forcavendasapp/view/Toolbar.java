a8ecd9ac22cacb8b5ef0fd32b4d59a6503dfebc0
BrunoVian <bruno.vian@edu.unipar.br>
2023-09-28 20:49:21 -0300

    primeiro commit

--- ForcaVendasApp/app/src/main/java/com/example/forcavendasapp/view/Toolbar.java
+++ ForcaVendasApp/app/src/main/java/com/example/forcavendasapp/view/Toolbar.java
@@ -3,20 +3,9 @@ package com.example.forcavendasapp.view;
 import android.os.Bundle;
 
 import com.example.forcavendasapp.databinding.ActivityToolbarBinding;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.forcavendasapp.view.databinding.ActivityToolbarBinding;
-
-import com.example.forcavendasapp.R;
 
 public class Toolbar extends AppCompatActivity {
 
